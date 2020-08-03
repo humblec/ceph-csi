1b42e02f312e74dc015f6d0867ad58d4b98b87dd
Humble Chirammal <hchiramm@redhat.com>
2020-08-03 15:50:51 +0530

    cephfs: add snapshot creation, restore, delete and clone features
    Signed-off-by: Humble Chirammal <hchiramm@redhat.com>

--- e2e/utils.go
+++ e2e/utils.go
@@ -283,7 +283,6 @@ func createCephfsStorageClass(c kubernetes.Interface, f *framework.Framework, en
 	Expect(stdErr).Should(BeEmpty())
 	// remove new line present in fsID
 	fsID = strings.Trim(fsID, "\n")
-
 	if clusterID != "" {
 		fsID = clusterID
 	}
@@ -926,6 +925,39 @@ func listRBDImages(f *framework.Framework) []string {
 		Fail(err.Error())
 	}
 	return imgInfos
+}
+
+func writeDataInPod(pvcPath, appPath string, f *framework.Framework) error {
+	pvc, err := loadPVC(pvcPath)
+	if pvc == nil {
+		return err
+	}
+
+	pvc.Namespace = f.UniqueName
+
+	app, err := loadApp(appPath)
+	if err != nil {
+		return err
+	}
+	app.Labels = map[string]string{"app": "write-data-in-pod"}
+	app.Namespace = f.UniqueName
+
+	err = createPVCAndApp("", f, pvc, app, deployTimeout)
+	if err != nil {
+		return err
+	}
+
+	opt := metav1.ListOptions{
+		LabelSelector: "app=write-data-in-pod",
+	}
+	// write data to PVC
+	filePath := app.Spec.Containers[0].VolumeMounts[0].MountPath + "/test"
+
+	_, writeErr := execCommandInPod(f, fmt.Sprintf("dd if=/dev/zero of=%s bs=1M count=500", filePath), app.Namespace, &opt)
+	Expect(writeErr).Should(BeEmpty())
+
+	err = deletePVCAndApp("", f, pvc, app)
+	return err
 }
 
 func checkDataPersist(pvcPath, appPath string, f *framework.Framework) error {
